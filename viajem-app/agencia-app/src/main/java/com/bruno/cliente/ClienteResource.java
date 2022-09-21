7818ecf1e08ad858247bc27851b6621d1a3ddfa7
NewarkX <38993322+NewarkX@users.noreply.github.com>
2022-09-21 19:04:48 -0300

    Criado agencia-app e agencia-cli

---
+++ viajem-app/agencia-app/src/main/java/com/bruno/cliente/ClienteResource.java
@@ -0,0 +1,41 @@
+package com.bruno.cliente;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/cliente")
+public class ClienteResource {
+
+    @Transactional
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newCliente(Cliente cliente){
+        cliente.id = null;
+        cliente.persist();
+        return Response.status(Response.Status.CREATED).entity(cliente).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Cliente> get(){
+        return Cliente.listAll();
+    }
+
+    @GET
+    @Path("findById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cliente findById(@QueryParam("id") long id){
+        return Cliente.findById(id);
+    }
+
+    @Transactional
+    @DELETE
+    @Path("deleteById")
+    public void deleteById(@QueryParam("id") long id){
+        Cliente.deleteById(id);
+    }
+}
---
+++ viajem-app/agencia-cli/src/main/java/com/bruno/reserva/Reserva.java
@@ -0,0 +1,26 @@
+package com.bruno.reserva;
+
+import com.bruno.cliente.Cliente;
+
+public class Reserva {
+
+    private Cliente cliente;
+
+    private Reserva(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public static Reserva of(Cliente cliente){
+        return new Reserva(cliente);
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    
+}
